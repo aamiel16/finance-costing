4f74e3872e99f424dc729d90272e60619ec7e089
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-02-01 01:03:00 +0800

    Added table for record listings

---
+++ src/components/lists/ListTable.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell, { TableCellProps } from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import ListTableToolbar from './ListTableToolbar';
+
+interface Column<DataType> {
+  id: string & keyof DataType;
+  label: string;
+  minWidth?: number;
+  align?: TableCellProps['align'];
+  format?: (value: number) => string;
+}
+
+interface Props<DataType> {
+  title: string;
+  columns: Column<DataType>[];
+  rows: DataType[];
+  keyField: string & keyof DataType;
+  pagination?: boolean;
+}
+
+function ListTable<DataType>(props: Props<DataType>) {
+  const { title, columns, rows, pagination = false, keyField } = props;
+
+  const classes = useStyles();
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (_, newPage: number) => {
+    if (!pagination) return;
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!pagination) return;
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <ListTableToolbar title={title} numSelected={0} />
+      <TableContainer className={classes.container}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .map(row => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={(row[keyField]) as any}>
+                    {columns.map(column => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {pagination && (
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      )}
+    </Paper>
+  );
+};
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  container: {
+    flex: 1
+  }
+});
+
+export default ListTable;
---
+++ src/components/lists/ListTableToolbar.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import clsx from "clsx";
+import {
+  createStyles,
+  lighten,
+  makeStyles,
+  Theme
+} from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import DeleteIcon from '@material-ui/icons/Delete';
+
+interface Props {
+  title: string;
+  numSelected: number;
+}
+
+const ListTableToolbar = (props: Props) => {
+  const classes = useStyles();
+  const { title = '', numSelected = 0 } = props;
+
+  return (
+    <Toolbar
+      className={clsx(classes.root, {
+        [classes.highlight]: numSelected > 0
+      })}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          className={classes.title}
+          color="inherit"
+          variant="subtitle1"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography className={classes.title} variant="h6" id="tableTitle">
+          {title}
+        </Typography>
+      )}
+      {!numSelected ? null : (
+        <Tooltip title="Delete">
+          <IconButton aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(1),
+    },
+    highlight:
+      theme.palette.type === 'light'
+        ? {
+            color: theme.palette.secondary.main,
+            backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+          }
+        : {
+            color: theme.palette.text.primary,
+            backgroundColor: theme.palette.secondary.dark,
+          },
+    title: {
+      flex: '1 1 100%',
+    },
+  }),
+);
+
+export default ListTableToolbar;
