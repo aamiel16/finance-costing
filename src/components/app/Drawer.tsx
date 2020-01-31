58ff02f8891fa3ea932b737c489a3efba648944b
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-02-01 01:01:10 +0800

    Fixed drawer to be always open

--- src/components/app/Drawer.tsx
+++ src/components/app/Drawer.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
+import { CSSProperties } from "@material-ui/core/styles/withStyles";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { default as MuiDrawer } from "@material-ui/core/Drawer";
 import { NavLink } from "react-router-dom";
 import List from "@material-ui/core/List";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -15,13 +12,11 @@ import { Metrics } from "../../constants";
 import { APP_ROUTES } from "../../routes/AppRoutes";
 
 interface Props {
-  isOpen: boolean;
-  onClose: () => void;
   children: React.ReactNode;
 }
 
 const Drawer: React.FC<Props> = props => {
-  const { onClose, isOpen, children } = props;
+  const { children } = props;
   const classes = useStyles();
 
   const renderNavigation = () => {
@@ -31,7 +26,6 @@ const Drawer: React.FC<Props> = props => {
           <ListItem
             key={path}
             button
-            className={classes.menuItem}
             exact
             to={path}
             component={NavLink}
@@ -55,26 +49,15 @@ const Drawer: React.FC<Props> = props => {
   };
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
+    <>
       <MuiDrawer
+        className={classes.drawer}
         variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: isOpen,
-          [classes.drawerClose]: !isOpen
-        })}
         classes={{
-          paper: clsx({
-            [classes.drawerOpen]: isOpen,
-            [classes.drawerClose]: !isOpen
-          })
+          paper: classes.drawerPaper,
         }}
       >
-        <div className={classes.toolbar}>
-          <IconButton onClick={onClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
+        <div className={classes.toolbar} />
         <Divider />
         <List>{renderNavigation()}</List>
       </MuiDrawer>
@@ -82,53 +65,31 @@ const Drawer: React.FC<Props> = props => {
         <div className={classes.toolbar} />
         {children}
       </main>
-    </div>
+    </>
   );
 };
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex"
-  },
-  drawer: {
-    width: Metrics.drawerWidth,
-    flexShrink: 0,
-    whiteSpace: "nowrap"
-  },
-  drawerOpen: {
-    width: Metrics.drawerWidth,
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  drawerClose: {
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    overflowX: "hidden",
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9) + 1
-    }
-  },
-  toolbar: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3)
-  },
-  menuItem: {
-    paddingLeft: theme.spacing(3)
-  },
-  selected: {
-    backgroundColor: "rgba(0, 0, 0, 0.08)"
-  }
-}));
+const useStyles = makeStyles((theme: Theme) => 
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      width: Metrics.drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: Metrics.drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+    selected: {
+      backgroundColor: "rgba(0, 0, 0, 0.08)"
+    },
+    toolbar: theme.mixins.toolbar as CSSProperties,
+  })
+);
+
 export default Drawer;
