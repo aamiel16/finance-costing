929c97f6a7c4a5af5e15fc04ef9c0c1ef8d81924
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-02-03 23:38:36 +0800

    Used page component in add item page

--- src/pages/item/ItemAddPage.tsx
+++ src/pages/item/ItemAddPage.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
-import Divider from "@material-ui/core/Divider";
-import Typography from "@material-ui/core/Typography";
 import { Formik } from "formik";
-import { Theme, createStyles, makeStyles } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import Page from "../../components/page/Page";
+import SaveFab from '../../components/buttons/SaveFab';
 
 interface Props {}
 
@@ -12,36 +12,44 @@ function ItemAddPage(props) {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.paper}>
-      <Typography variant="h5" noWrap>
-        New item
-      </Typography>
-      <Divider className={classes.divider} />
+    <Page title="Add item">
+      <SaveFab onSave={() => {}} />
       <Formik initialValues={{}} onSubmit={() => {}}>
         <>
-          <TextField className={classes.field} label="Name" variant="outlined" />
-          <TextField className={classes.field} label="Description" variant="outlined" />
-          <TextField className={classes.field} label="Supplier" variant="outlined" />
-          <TextField className={classes.field} label="Unit cost" variant="outlined" />
+          <Box marginY={1} width="30%">
+            <TextField
+              error
+              fullWidth
+              label="Name"
+              variant="outlined"
+              helperText="Required"
+            />
+          </Box>
+          <TextField
+            className={classes.field}
+            label="Description"
+            variant="outlined"
+          />
+          <TextField
+            className={classes.field}
+            label="Supplier"
+            variant="outlined"
+          />
+          <TextField
+            className={classes.field}
+            label="Unit cost"
+            variant="outlined"
+          />
         </>
       </Formik>
-    </Paper>
+    </Page>
   );
 }
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    paper: {
-      display: 'flex',
-      flexDirection: 'column',
-      padding: theme.spacing(3),
-      marginBottom: theme.spacing(3)
-    },
-    divider: {
-      marginTop: theme.spacing(3),
-      marginBottom: theme.spacing(3),
-    },
     field: {
+      marginTop: theme.spacing(),
       marginBottom: theme.spacing(),
     }
   })
