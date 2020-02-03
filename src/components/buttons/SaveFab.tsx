40005ef0f968159f06e6f74f50ef5d4aba39b7a9
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-02-03 23:52:04 +0800

    Fixed save FAB

--- src/components/buttons/SaveFab.tsx
+++ src/components/buttons/SaveFab.tsx
@@ -2,31 +2,16 @@ import React from "react";
 import isFn from 'lodash/isFunction';
 import Fab from './Fab';
 import SaveIcon from "@material-ui/icons/Save";
-import BackIcon from "@material-ui/icons/ChevronLeft";
-import { useHistory } from "react-router-dom";
 
 interface Props {
-  dirty?: boolean;
   onSave: () => void;
 }
 
-function AddFab(props: Props) {
-  const { dirty, onSave } = props;
-  const history = useHistory();
+function SaveFab(props: Props) {
+  const { onSave } = props;
 
   function handleClick(_: React.MouseEvent) {
-    if (dirty) {
-      isFn(onSave) && onSave();
-    } else {
-      history.goBack();
-    }
-  }
-
-  function renderButton() {
-    if (dirty) {
-      return <SaveIcon />
-    }
-    return <BackIcon />
+    isFn(onSave) && onSave();
   }
 
   return (
@@ -35,9 +20,9 @@ function AddFab(props: Props) {
       aria-label="add"
       onClick={handleClick}
     >
-      {renderButton()}
+      <SaveIcon />
     </Fab>
   );
 }
 
-export default AddFab;
+export default SaveFab;
