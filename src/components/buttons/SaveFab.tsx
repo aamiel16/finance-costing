45da22a81b6537a46f215dbafb4f48f5ab59f48d
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-02-04 00:56:05 +0800

    Added disabled option to Save FAB

--- src/components/buttons/SaveFab.tsx
+++ src/components/buttons/SaveFab.tsx
@@ -4,13 +4,15 @@ import Fab from './Fab';
 import SaveIcon from "@material-ui/icons/Save";
 
 interface Props {
+  disabled?: boolean;
   onSave: () => void;
 }
 
 function SaveFab(props: Props) {
-  const { onSave } = props;
+  const { disabled = false, onSave } = props;
 
   function handleClick(_: React.MouseEvent) {
+    if (disabled) return;
     isFn(onSave) && onSave();
   }
 
