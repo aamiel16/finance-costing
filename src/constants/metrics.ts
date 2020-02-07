b968694e5f223ae97f7bb72ef7a152bf78706d52
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-02-07 10:08:35 +0800

    Fixed maxWidth

--- src/constants/metrics.ts
+++ src/constants/metrics.ts
@@ -2,5 +2,5 @@ import {Theme} from "@material-ui/core/styles";
 
 export const Metrics = {
   drawerWidth: 240,
-  maxHeight: (theme: Theme) => (n = 2) => `calc(100vh - (${theme.mixins.toolbar.minHeight}px * ${n}))`
+  maxHeight: (theme: Theme) => (n = 2) => `calc(100vh - (${theme.mixins.toolbar.minHeight}px * (${n} + 0.1)))`
 }
