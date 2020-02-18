3924d59dccd88739416a258fa334ed036bf83ddc
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-02-18 22:30:11 +0800

    Added doctype types

--- src/constants/doctypes.ts
+++ src/constants/doctypes.ts
@@ -1,4 +1,10 @@
-export const DT_ITEM = 'item';
-export const DT_FEE = 'fee';
-export const DT_SHIPMENT_TERM = 'shipment-term';
-export const DT_SHIPPING_METHOD = 'shipping-method';
+export const DT_ITEM = "item";
+export const DT_FEE = "fee";
+export const DT_SHIPMENT_TERM = "shipment-term";
+export const DT_SHIPPING_METHOD = "shipping-method";
+
+export type Doctypes =
+  | typeof DT_ITEM
+  | typeof DT_FEE
+  | typeof DT_SHIPMENT_TERM
+  | typeof DT_SHIPPING_METHOD;
