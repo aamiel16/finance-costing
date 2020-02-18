83a137a2bf6ae1118c709c3d4b108fcee512b3ee
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-02-18 22:30:46 +0800

    Added common record list fetching

---
+++ src/hooks/common/makeUseFetchRecordList.ts
@@ -0,0 +1,71 @@
+import { useEffect, useState, useCallback, useMemo, useRef } from "react";
+import { ServiceContainer } from "../../services/common";
+import { Doctypes } from "../../constants";
+
+export function makeUseFetchRecordList(doctype: Doctypes) {
+  return () => {
+    const [list, setList] = useState({
+      rows: [],
+      totalRows: 0
+    });
+
+    const mountedRef = useRef(false);
+    const [loading, setLoading] = useState(false);
+    const [limit, setPageLimit] = useState(10);
+    const [prevCursor, setPrevCursor] = useState("");
+    const [currCursor, setCurrCursor] = useState("");
+
+    const processResult = useMemo(
+      () => ({ rows, totalRows, cursor }) => {
+        if (mountedRef.current) {
+          setList({ rows, totalRows });
+          setPrevCursor(currCursor);
+          setCurrCursor(cursor);
+          setLoading(false);
+        }
+      },
+      [currCursor]
+    );
+
+    const fetchFirst = useCallback(() => {
+      const service = ServiceContainer.get(doctype);
+      setCurrCursor("");
+      setLoading(true);
+      service.getList({ limit, cursor: "" }).then(processResult);
+    }, [limit, processResult]);
+
+    const fetchPrev = useCallback(() => {
+      const service = ServiceContainer.get(doctype);
+      setLoading(true);
+      service
+        .getList({ limit, cursor: prevCursor, descending: true })
+        .then(processResult);
+    }, [limit, prevCursor, processResult]);
+
+    const fetchNext = useCallback(() => {
+      const service = ServiceContainer.get(doctype);
+      setLoading(true);
+      service.getList({ limit, cursor: currCursor }).then(processResult);
+    }, [limit, currCursor, processResult]);
+
+    useEffect(() => {
+      mountedRef.current = true;
+      fetchFirst();
+    }, []); // eslint-disable-line
+
+    useEffect(() => {
+      if (currCursor) {
+        fetchFirst();
+      }
+    }, [limit]); // eslint-disable-line
+
+    return {
+      ...list,
+      loading,
+      setPageLimit,
+      fetchFirst,
+      fetchPrev,
+      fetchNext
+    };
+  };
+}
