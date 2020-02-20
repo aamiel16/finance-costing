6e3a7f6996a3eefb4c4ed7fd898cc9448dffdf02
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-02-20 22:38:08 +0800

    Added back button for Page component

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import BackIcon from "@material-ui/icons/ChevronLeft";
 import { PageHeader } from "./PageHeader";
 import { PageContainer } from "./PageContainer";
+import { useHistory } from "react-router-dom";
 
 export interface PageProps {
   title: string;
@@ -9,9 +12,26 @@ export interface PageProps {
 
 export function Page(props: PageProps) {
   const { title, children } = props;
+  const history = useHistory();
+
+  const renderBack = () => {
+    if (!history.length) {
+      return null;
+    }
+
+    return (
+      <IconButton onClick={history.goBack}>
+        <BackIcon />
+      </IconButton>
+    );
+  };
+
   return (
     <PageContainer>
-      <PageHeader>{title}</PageHeader>
+      <PageHeader>
+        {renderBack()}
+        {title}
+      </PageHeader>
       {children}
     </PageContainer>
   );
