b396785f18d1322f6061ff0195f1e7b37cdf834f
Adrian Sanchez <aamiel.sanchez@gmail.com>
2020-02-21 20:35:41 +0800

    Added yup ItemSchema

--- src/services/item/Item.model.ts
+++ src/services/item/Item.model.ts
@@ -1,10 +1,18 @@
-import { PouchDoc } from "../common";
+import * as yup from "yup";
+import { ToPouchDocument, ExtractYupSchema } from "../../types";
 
-export interface Item {
+export const ItemSchema = yup.object().shape({
+  name: yup.string().required(),
+  description: yup.string(),
+  unitcost: yup.string(),
+  supplier: yup.string()
+});
+
+export interface Item extends ExtractYupSchema<typeof ItemSchema> {
   name: string;
-  sku?: string;
   description?: string;
-  unit_cost?: string;
+  unitcost?: string;
+  supplier?: string;
 }
 
-export type ItemDoc = PouchDoc<Item>;
+export type ItemDocument = ToPouchDocument<Item>;
